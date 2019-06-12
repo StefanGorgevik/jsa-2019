c1bce1b96f0e8de50af87611a344f46f6e7abdeb
Stefan <stefangorgevik@hotmail.com>
2019-06-12 22:06:20 +0200

    cas_1
    Cas 1 Node.JS

---
+++ c1/index.js
@@ -0,0 +1,25 @@
+console.log("HI");
+
+name = "Stefan";
+var lastName = "Gorgevik";
+let age = 25;
+
+console.log("dadag " + lastName + " dadad")
+
+console.log(`dadag ${lastName} dadada`);
+
+console.log(name);
+
+function test() {
+    console.log(name);
+    console.log(lastName);
+    console.log(age);
+    a = 10;
+    // var b = 20;
+    // let c = 30;
+}
+
+test();
+console.log(a);
+// console.log(b);
+// console.log(c);
