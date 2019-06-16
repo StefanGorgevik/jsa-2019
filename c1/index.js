914b696ce7463bf38bbe35fdf2196846e9e2bb97
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-06-16 22:50:58 +0200

    Update

--- c1/index.js
+++ c1/index.js
@@ -1,25 +1,25 @@
 console.log("HI");
 
-name = "Stefan";
-var lastName = "Gorgevik";
-let age = 25;
+// name = "Stefan";
+// var lastName = "Gorgevik";
+// let age = 25;
 
-console.log("dadag " + lastName + " dadad")
+// console.log("dadag " + lastName + " dadad")
 
-console.log(`dadag ${lastName} dadada`);
+// console.log(`dadag ${lastName} dadada`);
 
-console.log(name);
+// console.log(name);
 
-function test() {
-    console.log(name);
-    console.log(lastName);
-    console.log(age);
-    a = 10;
-    // var b = 20;
-    // let c = 30;
-}
+// function test() {
+//     console.log(name);
+//     console.log(lastName);
+//     console.log(age);
+//     a = 10;
+//     // var b = 20;
+//     // let c = 30;
+// }
 
-test();
-console.log(a);
+// test();
+// console.log(a);
 // console.log(b);
 // console.log(c);
