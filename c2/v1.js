287ddaadc10aefc0b09534267c7865c6ab4a122e
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-06-18 01:09:16 +0200

    Update v1.js

--- c2/v1.js
+++ c2/v1.js
@@ -18,45 +18,51 @@
 // };
 // everyThird();
 
-function everyThird(c) {
-    var i = 0;
-    var j = 0;
-    while(i < 100) {
-        i++;
-        if(i % 2 === 0){
-            j++;                 
-        }
-        if(i % 2 === 0 && j === c) {
-            console.log("1" + i);  
-            j = 0;
-        }
-    }
-};
+// function everyThird(c) {
+//     var i = 0;
+//     var j = 0;
+//     while(i < 100) {
+//         i++;
+//         if(i % 2 === 0){
+//             j++;                 
+//         }
+//         if(i % 2 === 0 && j === c) {
+//             console.log("1" + i);  
+//             j = 0;
+//         }
+//     }
+// };
 //everyThird(4);
 
 
 
 
 // //exercise 2   
-// var arr = [];
-// var array = [248,21,68,239,46];
+var arr = [];
+var array = [248,21,68,239,46]; //14,3,14,14,10; 5,3,5,5,1, --19
 function arraySum(niza) {
     var zbir = 0;
     var num;
     for(var j = 0; j < niza.length; j++){
         num = 0;
         num = niza[j];
-        var digits = (""+num).split("").map(Number) ;
-        for(var i = 0; i < digits.length; i++) {
-            zbir += digits[i];
+        var digits = (""+num).split("").map(Number);
+        console.log("This is digit: " + digits);
+    for(var i = 0; i < digits.length; i++) {
+        if(digits.length >= 1) {
+            zbir += digits[i];  
+            arr.push(zbir);
+            console.log("arr after push: " + arr);
+            array.shift();
+            console.log("this is array after shift: " + array);
         }
-        arr.push(zbir);
-        zbir = 0;
-    }
-    return arr;
+    }  }
+    // zbir = 0;
+    console.log(arr);
+    return arr;  
 }
-// arraySum(array);
-// console.log(arr);
+arraySum(array);
+console.log(arr);
 //arraySum(arr);
 
 
@@ -66,6 +72,16 @@ function arraySum(niza) {
 
 //19
 
+
+// for(var i = 0; i < digits.length; i++) {
+//     // if(digits[i].length > 1) {
+//         zbir += digits[i];  
+//         arr.push(zbir);
+//         console.log("arr after push: " + arr);
+//         array.shift();
+//         console.log("this is array after shift: " + array);
+//     // }
+// }  }
 
 // function arrSum(niza) {
 //     var c = 0;
@@ -86,7 +102,7 @@ function arraySum(niza) {
 
 //homework
 
-module.exports = {
-    arraySum,
-    everyThird
-};
+// module.exports = {
+//     arraySum,
+//     everyThird
+// };
