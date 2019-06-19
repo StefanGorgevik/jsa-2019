fed09dace320d80f17fce1902573ea8464b22768
Stefan <stefangorgevik@hotmail.com>
2019-06-19 22:00:54 +0200

    Update v2.js

--- c3/v2.js
+++ c3/v2.js
@@ -10,23 +10,30 @@ var fs = require("fs");
 //     }
 // });
 
+fs.readFile("test.txt", "utf8", (err,data) => { // without utf === <Buffer 4e 6f 64 65 2e 6a 73 20 69 73 20 74 68 65 20 62 65 73 74>
+    if(err) {
+        console.log(err);
+    }
+    console.log(data);
+})
+
 //Callback func
 
-var pero = (cb) => {
-    var sum = 0;
-    for(var i = 0; i < 100; i++){
-        sum += i;
-    }
-    cb(sum);
-}
+// var pero = (cb) => {
+//     var sum = 0;
+//     for(var i = 0; i < 100; i++){
+//         sum += i;
+//     }
+//     cb(sum);
+// }
 
-//1
-// log((num) => {
+// //1
+// // log((num) => {
+// //     console.log(num);
+// // })
+// //same as
+// var log = (num) => {
 //     console.log(num);
-// })
-//same as
-var log = (num) => {
-    console.log(num);
-}
+// }
 
-pero(log);
+// pero(log);
